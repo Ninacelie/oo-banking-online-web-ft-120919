dde59b979e0b2aff73868c66ab6fa062cfb90ec9
Nina Angelo <ninacelie@gmail.com>
2020-01-08 03:16:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,12 +19,12 @@ class Transfer
     end
 
     def execute_transaction
-      if !self.valid? && @sender.balance <= @amount 
-      @status = "rejected" 
+      if !self.valid? && @sender.balance <= @amount
+      @status = "rejected"
       @status = "Transaction rejected. Please check your account balance."
     elsif @status == "pending"
     end
-  
+
 
 
 end
