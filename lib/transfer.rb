456456a5e7639a172915da45bdd0b2b19c4a2236
Nina Angelo <ninacelie@gmail.com>
2020-01-08 01:49:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,7 @@
 class Transfer
-  require 'pry'
 
   attr_accessor :amount, :status, :sender, :receiver
 
-    binding.pry 
     def initialize(amount, sender, receiver)
       @sender = sender
       @receiver = receiver
