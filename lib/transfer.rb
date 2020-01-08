a7431b9c31f40c74c794e7bd5b77f86eca9f3aca
Nina Angelo <ninacelie@gmail.com>
2020-01-08 02:48:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
          @sender.balance -= @amount
          @receiver.balance += @amount
          @status = "complete"
-      elsif
+      else 
         @status = "rejected"
         return "Transaction rejected. Please check your account balance."
        end
